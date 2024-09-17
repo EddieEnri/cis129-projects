115fbc59db627c8402abc2f9ed75d5a46b8025c7
EddieEnri <eenriquez14@mail.pima.edu>
2024-09-16 21:46:49 -0700

    Update cis129_lab02_shortStory.py
    Added a poem.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,9 @@
-print('Hello, World!')
+# Author: Eddie Enriquez
+# My first Python lab in my CIS 129 course
+# Prints a simple short story
+
+print('There once was a man named Dan.')
+print('Dan was a really tall man.')
+print("He didn't fit in his shoes.")
+print('His toes were always bruised.')
+print('He now looks like a caveman.')
